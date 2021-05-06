ef9f8483a509d514dc937712643edd24a1140655
manas-droid <manas.kalangan@gmail.com>
2021-05-06 21:02:22 +0530

    Successfully able to downloadable using executors

--- app/src/main/java/com/example/videostreamingapp/ui/Fragments/VideoFragment.java
+++ app/src/main/java/com/example/videostreamingapp/ui/Fragments/VideoFragment.java
@@ -1,20 +1,19 @@
 package com.example.videostreamingapp.ui.Fragments;
 
 import android.Manifest;
-import android.app.DownloadManager;
-import android.content.BroadcastReceiver;
-import android.content.ContentResolver;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.net.Uri;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.preference.PreferenceManager;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -22,7 +21,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.MimeTypeMap;
 import android.widget.MediaController;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -31,31 +29,50 @@ import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.videostreamingapp.R;
 import com.example.videostreamingapp.VideosAPI.Videos;
 
-import static android.app.DownloadManager.ACTION_NOTIFICATION_CLICKED;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 
 public class VideoFragment extends Fragment {
-    Videos video;
-    TextView title , subTitle , description;
-    VideoView videoView;
-    ProgressBar progressBar;
-    private static int WRITE_PERMISSION = 1001;
-    private static final String TAG = "VideoFragment";
+    private Videos video;
+    private TextView title , subTitle , description;
+    private VideoView videoView;
+    private ProgressBar progressBar;
+    private  final int WRITE_PERMISSION = 1001;
+    private  static final String TAG = "VideoFragment";
     private String fileName;
-    private String URL;
-    private DownloadManager manager;
-    private final String DOWNLOAD_ID ="DOWNLOAD_ID";
-   private SharedPreferences preferences;
+    private String url;
+    private final int DOWNLOAD_NOTIFICATION_ID = 1221;
+
+    private AtomicBoolean isCancelled;
+    private Handler handler;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Bundle bundle = getArguments();
         video = (Videos) bundle.getSerializable("video");
+        isCancelled = new AtomicBoolean(false);
+        handler = new Handler(Looper.getMainLooper());
         setHasOptionsMenu(true);
     }
 
@@ -80,7 +97,7 @@ public class VideoFragment extends Fragment {
         title.setText(video.getTitle());
         subTitle.setText(video.getSubtitle());
         description.setText(video.getDescription());
-        URL = video.getSources().get(0);
+        url = video.getSources().get(0);
 
         videoView.setVideoPath(video.getSources().get(0));
         MediaController mediaController  = new MediaController(getContext());
@@ -104,93 +121,104 @@ public class VideoFragment extends Fragment {
 
         switch (item.getItemId()){
             case R.id.downLoad :
-                Toast.makeText(getContext(), "Download Video Clicked", Toast.LENGTH_SHORT).show();
                 startDownLoad();
                 break;
         }
         return true;
     }
 
+
     public void startDownLoad(){
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
             if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
-                downloadFile(fileName ,URL );
+                downloadFile(fileName ,url );
             }
             else{
                 requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_PERMISSION);
             }
         }else{
-            downloadFile(fileName , URL);
+            downloadFile(fileName , url);
         }
     }
 
-    public void downloadFile(String fileName , String URL){
-        Uri downLoadURI = Uri.parse(URL);
-         manager = (DownloadManager) getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
-        try {
-            if(manager!=null){
-                DownloadManager.Request request = new DownloadManager.Request(downLoadURI);
-                request.allowScanningByMediaScanner();
-                request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE)
-                       .setTitle(fileName)
-                        .setDescription("Downloading "+fileName)
-                        .setAllowedOverRoaming(true)
-                        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-                        .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName)
-                        .setMimeType(getMimeType(downLoadURI));
-               long download_id = manager.enqueue(request);
-               preferences  = PreferenceManager.getDefaultSharedPreferences(getContext());
-                SharedPreferences.Editor prefEdit = preferences.edit();
-                prefEdit.putLong(DOWNLOAD_ID, download_id);
-                prefEdit.apply();
-                Toast.makeText(getContext(), "Download Started", Toast.LENGTH_SHORT).show();
-            }else{
-                Intent intent = new Intent(Intent.ACTION_VIEW , downLoadURI);
-                startActivity(intent);
 
+
+
+    public void downloadFile(String fileName , String url){
+        Log.d(TAG, "downloadFile: here");
+        ExecutorService executorService =Executors.newSingleThreadExecutor();
+        executorService.submit(() -> {
+            InputStream input = null;
+            OutputStream output = null;
+            HttpURLConnection connection = null;
+            try{
+                URL link = new URL(url);
+                connection =(HttpURLConnection) link.openConnection();
+                connection.connect();
+
+                if(connection.getResponseCode() != HttpURLConnection.HTTP_OK){
+                    return "Server returned HTTP "+connection.getResponseCode()+" "+connection.getResponseMessage();
+                }
+
+                int fileLength = connection.getContentLength();
+                input = connection.getInputStream();
+                File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) , fileName+".mp4");
+                output = new FileOutputStream(file);
+                byte data [] = new byte[1024*4];
+                long total = 0;
+                int count;
+                while((count = input.read(data))!= -1){
+                    if(isCancelled.get()){
+                        input.close();
+                        return null;
+                    }
+
+                    total+=count;
+                    int percentage =(int) total * 100 / fileLength;
+                    if(fileLength>0) {
+                        publishProgress(percentage);
+                    }
+                    output.write(data, 0, count);
+                }
+                return "file length "+fileLength;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                try {
+                    if(output!=null)
+                        output.close();
+                    if(input!=null)
+                        input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if(connection!=null)connection.disconnect();
             }
+            return null;
+        });
+        executorService.shutdown();
+    }
 
-        }catch(Exception e){
-            Toast.makeText(getContext(), "Something went wrong!", Toast.LENGTH_SHORT).show();
-            Log.e(TAG, "downloadFile: "+e.getMessage());
-        }
+    public void publishProgress(int progress){
+        handler.post(()->{
+            Log.d(TAG, "publishProgress: "+progress+"  thread name "+Thread.currentThread().getName());
+        });
 
     }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if(requestCode == WRITE_PERMISSION){
             if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                downloadFile(fileName, URL);
+                downloadFile(fileName, url);
             }else{
                 Toast.makeText(getActivity(), "Permission Denied!", Toast.LENGTH_SHORT).show();
             }
         }
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        IntentFilter intentFilter = new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
-        getActivity().registerReceiver(downloadReceiver, intentFilter);
-    }
-
-    private String getMimeType(Uri uri){
-        ContentResolver resolver = getActivity().getContentResolver();
-        MimeTypeMap mimeTypeMap =  MimeTypeMap.getSingleton();
-        return mimeTypeMap.getExtensionFromMimeType(resolver.getType(uri));
-    }
-    private BroadcastReceiver downloadReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            DownloadManager.Query query = new DownloadManager.Query();
-            query.setFilterById(preferences.getLong(DOWNLOAD_ID, 0));
-            Cursor cursor = manager.query(query);
-            if(cursor.moveToFirst()){
-
-            }
-        }
-    };
 
 }
